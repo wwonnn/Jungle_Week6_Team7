af8ce3078c143c86434e8155ef589a6fa094d658
JunHyeop3631 <sstechangle@gmail.com>
2026-03-31 00:25:45 +0900

    feat: UMaterial 캐싱 적용 및 중복 파싱 방지 로직 추가

--- CraftonEngine/Source/Engine/Mesh/ObjManager.h
+++ CraftonEngine/Source/Engine/Mesh/ObjManager.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 #include "Core/CoreTypes.h"
 #include "Object/ObjectIterator.h"
@@ -10,16 +10,20 @@
 struct FStaticMesh;
 struct FStaticMaterial;
 class UStaticMesh;
+class UMaterial;
 
 // 에셋 로드/캐싱 관리자
 class FObjManager
 {
 	// path → UStaticMesh* 캐시 (소유권은 UObjectManager)
 	static std::map<std::string, UStaticMesh*> StaticMeshCache;
+	static TMap<FString, UMaterial*> MaterialCache;
+
 
 public:
 	static std::string GetBinaryFilePath(const std::string& OriginalPath);
 	static UStaticMesh* LoadObjStaticMesh(const std::string& PathFileName, ID3D11Device* InDevice);
+	static UMaterial* GetOrLoadMaterial(const FString& MaterialName);
 
 private:
 	static bool LoadStaticMeshAsset(const std::string& PathFileName, ID3D11Device* InDevice,
