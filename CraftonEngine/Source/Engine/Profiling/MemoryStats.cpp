7f354b131e9be7b87e13b882c1aea1d3d3b6f99a
Seyoung Park <seyoungpark@hanyang.ac.kr>
2026-03-31 11:09:22 +0900

    feat: add memory stat info

--- CraftonEngine/Source/Engine/Profiling/MemoryStats.cpp
+++ CraftonEngine/Source/Engine/Profiling/MemoryStats.cpp
@@ -1,6 +1,10 @@
-#include "MemoryStats.h"
+﻿#include "MemoryStats.h"
 
 uint32 MemoryStats::TotalAllocationBytes = 0;
 uint32 MemoryStats::TotalAllocationCount = 0;
 uint32 MemoryStats::PixelShaderMemory = 0;
 uint32 MemoryStats::VertexShaderMemory = 0;
+uint32 MemoryStats::TextureMemory = 0;
+uint32 MemoryStats::VertexBufferMemory = 0;
+uint32 MemoryStats::IndexBufferMemory = 0;
+uint32 MemoryStats::ConstantBufferMemory = 0;
