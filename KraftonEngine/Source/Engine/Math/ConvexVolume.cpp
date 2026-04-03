36261b3c0f51eb5f1751ffbb9f0c9d4aba5bf196
wwonnn <dnjsgml0508@naver.com>
2026-04-04 01:55:35 +0900

    feat: Frustum Math

---
+++ KraftonEngine/Source/Engine/Math/ConvexVolume.cpp
@@ -0,0 +1,40 @@
+﻿#include <Engine/Math/ConvexVolume.h>
+#include <Engine/Core/EngineTypes.h>
+
+void FConvexVolume::UpdateFromMatrix(const FMatrix& InViewProjectionMatrix)
+{
+	const FMatrix& M = InViewProjectionMatrix;
+
+	// Left
+	Planes[0] = { M.M[0][3] + M.M[0][0], M.M[1][3] + M.M[1][0], M.M[2][3] + M.M[2][0], M.M[3][3] + M.M[3][0] };
+	// Right
+	Planes[1] = { M.M[0][3] - M.M[0][0], M.M[1][3] - M.M[1][0], M.M[2][3] - M.M[2][0], M.M[3][3] - M.M[3][0] };
+
+	// Top
+	Planes[2] = { M.M[0][3] - M.M[0][1], M.M[1][3] - M.M[1][1], M.M[2][3] - M.M[2][1], M.M[3][3] - M.M[3][1] };
+	// Bottom
+	Planes[3] = { M.M[0][3] + M.M[0][1], M.M[1][3] + M.M[1][1], M.M[2][3] + M.M[2][1], M.M[3][3] + M.M[3][1] };
+
+	// Near
+	Planes[4] = { M.M[0][2], M.M[1][2], M.M[2][2], M.M[3][2] };
+	// Far
+	Planes[5] = { M.M[0][3] - M.M[0][2], M.M[1][3] - M.M[1][2], M.M[2][3] - M.M[2][2], M.M[3][3] - M.M[3][2] };
+
+	for (auto& P : Planes) {
+		P.Normalize();
+	}
+}
+
+bool FConvexVolume::IntersectAABB(const FBoundingBox& Box) const
+{
+	for (const auto& P : Planes) {
+		FVector4 PVertex = Box.Min;
+		if (P.X >= 0) PVertex.X = Box.Max.X;
+		if (P.Y >= 0) PVertex.Y = Box.Max.Y;
+		if (P.Z >= 0) PVertex.Z = Box.Max.Z;
+
+		if (P.Dot(PVertex) + P.Length() < 0)
+			return false;
+	}
+	return true;
+}
---
+++ KraftonEngine/Source/Engine/Math/ConvexVolume.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include "Vector.h"
+#include "Matrix.h"
+
+struct FBoundingBox;
+
+struct FConvexVolume
+{
+public:
+	void UpdateFromMatrix(const FMatrix& InViewProjectionMatrix);
+	bool IntersectAABB(const FBoundingBox& Box) const;
+
+private:
+	FVector4 Planes[6];
+};
