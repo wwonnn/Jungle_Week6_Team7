3bf2ad8187ccb175a084603be92ab65be935e0c3
LEE SangHoon <hoon15x@gmail.com>
2026-04-04 10:45:09 +0900

    refactor: BVH to new file
    계획대로 이관했습니다. 이제 고도화 작업 예정입니다.

---
+++ KraftonEngine/Source/Engine/Collision/PickingBVH.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+
+#include "Engine/Core/CoreTypes.h"
+#include "Core/RayTypes.h"
+#include "Core/CollisionTypes.h"
+#include "Core/EngineTypes.h"
+
+class AActor;
+class UPrimitiveComponent;
+
+class FPickingBVH
+{
+public:
+	void MarkDirty();
+	void BuildNow(const TArray<AActor*>& Actors);
+	void EnsureBuilt(const TArray<AActor*>& Actors);
+	bool Raycast(const FRay& Ray, FHitResult& OutHitResult, AActor*& OutActor) const;
+
+private:
+	struct FLeaf
+	{
+		FBoundingBox Bounds;
+		UPrimitiveComponent* Primitive = nullptr;
+		AActor* Owner = nullptr;
+	};
+
+	struct FNode
+	{
+		FBoundingBox Bounds;
+		int32 LeftChild = -1;
+		int32 RightChild = -1;
+		int32 FirstLeaf = 0;
+		int32 LeafCount = 0;
+
+		bool IsLeaf() const { return LeftChild < 0 && RightChild < 0; }
+	};
+
+	int32 BuildRecursive(int32 Start, int32 End);
+
+	bool bDirty = true;
+	TArray<FLeaf> Leaves;
+	TArray<FNode> Nodes;
+};
