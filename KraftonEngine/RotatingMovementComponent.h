99bf6fd32b664d8c9d34f7ded0606630b4a67d4b
LEE SangHoon <havel15x@naver.com>
2026-04-08 14:48:06 +0900

    feat: add rotate component
    회전. 테스트 필요.

---
+++ KraftonEngine/RotatingMovementComponent.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include "Component/MovementComponent.h"
+#include "Math/Rotator.h"
+
+class URotatingMovementComponent : public UMovementComponent
+{
+public:
+	DECLARE_CLASS(URotatingMovementComponent, UMovementComponent)
+
+	void TickComponent(float DeltaTime) override;
+	void GetEditableProperties(TArray<FPropertyDescriptor>& OutProps) override;
+
+private:
+	FRotator RotationRate = FRotator(0.0f, 90.0f, 0.0f);
+};
