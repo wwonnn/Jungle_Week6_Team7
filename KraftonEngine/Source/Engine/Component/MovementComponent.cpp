fcba69d4141091fbdc1c447ed4c760df01ee03ac
LEE SangHoon <havel15x@naver.com>
2026-04-08 14:30:48 +0900

    feat: add movement component
    향후 여러 이동 로직이 따라붙을 클래스 구현

---
+++ KraftonEngine/Source/Engine/Component/MovementComponent.cpp
@@ -0,0 +1,46 @@
+﻿#include "Component/MovementComponent.h"
+
+#include "Component/SceneComponent.h"
+#include "GameFramework/AActor.h"
+#include "Object/ObjectFactory.h"
+
+IMPLEMENT_CLASS(UMovementComponent, UActorComponent)
+
+void UMovementComponent::BeginPlay()
+{
+	UActorComponent::BeginPlay();
+	TryAutoRegisterUpdatedComponent();
+}
+
+void UMovementComponent::TickComponent(float DeltaTime)
+{
+	//일단 여타 컴포넌트들과 마찬가지로 처리. 비어 있음.
+	UActorComponent::TickComponent(DeltaTime);
+}
+
+void UMovementComponent::GetEditableProperties(TArray<FPropertyDescriptor>& OutProps)
+{
+	UActorComponent::GetEditableProperties(OutProps);
+	OutProps.push_back({ "Auto Register Updated", EPropertyType::Bool, &bAutoRegisterUpdatedComponent });
+}
+
+void UMovementComponent::SetUpdatedComponent(USceneComponent* NewUpdatedComponent)
+{
+	UpdatedComponent = NewUpdatedComponent;
+}
+
+void UMovementComponent::TryAutoRegisterUpdatedComponent()
+{
+	if (!bAutoRegisterUpdatedComponent || UpdatedComponent != nullptr)
+	{
+		return;
+	}
+
+	AActor* OwnerActor = GetOwner();
+	if (!OwnerActor)
+	{
+		return;
+	}
+
+	SetUpdatedComponent(OwnerActor->GetRootComponent());
+}
---
+++ KraftonEngine/Source/Engine/Component/MovementComponent.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include "Component/ActorComponent.h"
+
+class USceneComponent;
+
+/**
+ * 런타임(PIE, Game mode) 동안
+ * USceneComponet을 움직이는 로직들의 베이스 클래스.
+ * 실제 이동 로직은 자식 클래스에서 담당합니다.
+ */
+class UMovementComponent : public UActorComponent
+{
+public:
+	DECLARE_CLASS(UMovementComponent, UActorComponent)
+
+	void BeginPlay() override;
+	void TickComponent(float DeltaTime) override;
+	void GetEditableProperties(TArray<FPropertyDescriptor>& OutProps) override; //에디터 패널에 띄울 프로퍼티
+
+	void SetUpdatedComponent(USceneComponent* NewUpdatedComponent);
+	USceneComponent* GetUpdatedComponent() const { return UpdatedComponent; }
+	bool HasValidUpdatedComponent() const { return UpdatedComponent != nullptr; }
+
+protected:
+	void TryAutoRegisterUpdatedComponent();
+
+protected:
+	USceneComponent* UpdatedComponent = nullptr; //움직일 대상
+	bool bAutoRegisterUpdatedComponent = true;
+};
